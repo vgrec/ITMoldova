fb5f10da8cd0d624cb6a6c60645f1553d15de2a6
Veaceslav Grec <grec.vs@gmail.com>
2016-08-14 12:12:14 +0200

    Create ContentParser

---
+++ app/src/main/java/com/itmoldova/parser/ContentParser.java
@@ -0,0 +1,56 @@
+package com.itmoldova.parser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This class parses the HTML content of an article and splits it into a list of {@link Block}s.
+ * Then based on this list, the appropriate views are created in the UI.
+ */
+public class ContentParser {
+    private static final String IMG_START_TAG = "<img";
+    private static final String IMG_END_TAG = "/>";
+    private static final String SRC_START_TAG = "src=\"";
+    private static final String SRC_END_TAG = "\"";
+
+
+    public static List<Block> parse(String content) {
+        List<Block> blocks = new ArrayList<>();
+
+        int startTag;
+        while ((startTag = content.indexOf(IMG_START_TAG)) != -1) {
+            int endTag = content.indexOf(IMG_END_TAG, startTag);
+            if (endTag != -1) {
+                String text = content.substring(0, startTag);
+                if (text.length() > 0) {
+                    blocks.add(new Block(Block.Type.TEXT, text));
+                }
+
+                String imageTag = content.substring(startTag, endTag + IMG_END_TAG.length());
+                String imageUrl = extractUrl(imageTag);
+                blocks.add(new Block(Block.Type.IMAGE, imageUrl));
+                content = content.substring(endTag + IMG_END_TAG.length(), content.length());
+            }
+        }
+
+        // Add the remaining text (if any)
+        if (content.length() > 0) {
+            blocks.add(new Block(Block.Type.TEXT, content));
+        }
+
+        return blocks;
+    }
+
+    public static String extractUrl(String imageTag) {
+        String url = null;
+        int startTag = imageTag.indexOf(SRC_START_TAG);
+        if (startTag != -1) {
+            int endTag = imageTag.indexOf(SRC_END_TAG, startTag + SRC_START_TAG.length());
+            if (endTag != -1) {
+                url = imageTag.substring(startTag + SRC_START_TAG.length(), endTag);
+            }
+        }
+
+        return url;
+    }
+}
---
+++ app/src/test/java/com/itmoldova/ContentParserTest.java
@@ -0,0 +1,80 @@
+package com.itmoldova;
+
+import com.itmoldova.parser.Block;
+import com.itmoldova.parser.ContentParser;
+
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Unit tests for {@link ContentParser} class
+ */
+public class ContentParserTest {
+
+    private static final String TEXT_1 = "<p>De fiecare dată când se dezvoltă o noua linie de smartphone-uri Nexus, Google vine cu un nume într-un fel sau altul asociat cu vietățile marine.";
+    private static final String TEXT_2 = "<p><a href=\"http://itmoldova.com/wp-content/uploads/2016/08/Meizu-M3E-2.png\">";
+    private static final String TEXT_3 = "</a><p style=\"text-align: justify;\"><span class=\"notranslate\"> Gadgetul dispune de un procesor MediaTek Helio P10";
+
+    private static final String IMAGE_1 = "http://itmoldova.com/wp-content/uploads/2016/08/Meizu-M3E-2-403x1024.png";
+    private static final String IMAGE_2 = "http://itmoldova.com/wp-content/uploads/2016/08/Meizu-M3E-3-396x1024.png";
+    private static final String IMAGE_3 = "http://itmoldova.com/wp-content/uploads/2016/08/Meizu-M3E-5-615x591.png";
+
+    private static final String IMAGE_TAG_1 = "<img class=\"aligncenter size-large wp-image-110474\" src=\"" + IMAGE_1 + "\" alt=\"Meizu M3E 2\" width=\"403\" height=\"1024\" />";
+    private static final String IMAGE_TAG_2 = "<img class=\"aligncenter size-large wp-image-110475\" src=\"" + IMAGE_2 + "\" alt=\"Meizu M3E 3\" width=\"396\" height=\"1024\" />";
+    private static final String IMAGE_TAG_3 = "<img class=\"aligncenter size-large wp-image-110476\" src=\"" + IMAGE_3 + "\" alt=\"Meizu M3E 5\" width=\"615\" height=\"591\" />";
+
+    private static final String CONTENT_ONLY_TEXT = TEXT_1;
+    private static final String CONTENT_IMAGE_FIRST_TEXT_AFTER = IMAGE_TAG_1 + TEXT_1;
+    private static final String CONTENT_TEXT_FIRST_IMAGE_AFTER = TEXT_1 + IMAGE_TAG_1;
+
+    private static final String CONTENT_MIXED = TEXT_1
+            + IMAGE_TAG_1
+            + TEXT_2
+            + IMAGE_TAG_2
+            + TEXT_3
+            + IMAGE_TAG_3;
+
+    @Test
+    public void testExtractUrl() {
+        String url = ContentParser.extractUrl(IMAGE_TAG_1);
+        assertThat(url).isEqualTo(IMAGE_1);
+    }
+
+    @Test
+    public void testContentParser_OnlyText() {
+        List<Block> parts = ContentParser.parse(CONTENT_ONLY_TEXT);
+        assertThat(parts.size()).isEqualTo(1);
+        assertThat(parts.get(0).getContent()).isEqualTo(TEXT_1);
+    }
+
+    @Test
+    public void testContentParser_ImageFirstTextAfter() {
+        List<Block> parts = ContentParser.parse(CONTENT_IMAGE_FIRST_TEXT_AFTER);
+        assertThat(parts.size()).isEqualTo(2);
+        assertThat(parts.get(0).getContent()).isEqualTo(IMAGE_1);
+        assertThat(parts.get(1).getContent()).isEqualTo(TEXT_1);
+    }
+
+    @Test
+    public void testContentParser_TextFirstImageAfter() {
+        List<Block> parts = ContentParser.parse(CONTENT_TEXT_FIRST_IMAGE_AFTER);
+        assertThat(parts.size()).isEqualTo(2);
+        assertThat(parts.get(0).getContent()).isEqualTo(TEXT_1);
+        assertThat(parts.get(1).getContent()).isEqualTo(IMAGE_1);
+    }
+
+    @Test
+    public void testContentParser_Mixed() {
+        List<Block> parts = ContentParser.parse(CONTENT_MIXED);
+        assertThat(parts.size()).isEqualTo(6);
+        assertThat(parts.get(0).getContent()).isEqualTo(TEXT_1);
+        assertThat(parts.get(1).getContent()).isEqualTo(IMAGE_1);
+        assertThat(parts.get(2).getContent()).isEqualTo(TEXT_2);
+        assertThat(parts.get(3).getContent()).isEqualTo(IMAGE_2);
+        assertThat(parts.get(4).getContent()).isEqualTo(TEXT_3);
+        assertThat(parts.get(5).getContent()).isEqualTo(IMAGE_3);
+    }
+}
