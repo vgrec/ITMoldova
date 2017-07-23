b90e1a78c81309eb9e94d9949eea0ab349e60cb4
Veaceslav Grec <grec.vs@gmail.com>
2017-07-23 14:16:51 +0200

    Change icons in navigation drawer; Update gradle version.

--- app/src/main/java/com/itmoldova/list/MainActivity.java
+++ app/src/main/java/com/itmoldova/list/MainActivity.java
@@ -71,17 +71,9 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.action_settings) {
             startActivity(new Intent(this, SettingsActivity.class));
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.action_bookmarks) {
 
         }
 
