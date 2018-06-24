492d586dc0dac92fa0f4843f10ce005a83eeeb90
vgrec <grec.vs@gmail.com>
2018-06-24 17:18:36 +0200

    Check string resources, make default Romanian

--- app/src/main/java/com/itmoldova/model/Category.java
+++ app/src/main/java/com/itmoldova/model/Category.java
@@ -8,17 +8,17 @@ import com.itmoldova.R;
  * Author vgrec, on 11.09.16.
  */
 public enum Category {
-    HOME("home", R.string.home),
-    IT_WORLD("it-world", R.string.it_world),
-    IT_NEWS_MD("it-news-md", R.string.it_news_md),
-    SOFTWARE("noutati-software", R.string.software),
-    HARDWARE("noutati-hardware", R.string.hardware),
-    JOCURI("game-news", R.string.jocuri),
-    SECURITATE("securitate-2", R.string.securitate),
-    SOFT_GRATIS("soft-gratis", R.string.soft_gratis),
-    SFATURI_UTILE("sfaturi-utile", R.string.sfaturi_utile),
-    ISTORIA_IT("istoria-it", R.string.istoria_it),
-    FUNNY_IT("funny-it", R.string.funny_it);
+    HOME("home", R.string.category_home),
+    IT_WORLD("it-world", R.string.category_it_world),
+    IT_NEWS_MD("it-news-md", R.string.category_it_news_md),
+    SOFTWARE("noutati-software", R.string.category_software),
+    HARDWARE("noutati-hardware", R.string.category_hardware),
+    JOCURI("game-news", R.string.category_games),
+    SECURITATE("securitate-2", R.string.category_security),
+    SOFT_GRATIS("soft-gratis", R.string.category_free_software),
+    SFATURI_UTILE("sfaturi-utile", R.string.category_tips),
+    ISTORIA_IT("istoria-it", R.string.category_history),
+    FUNNY_IT("funny-it", R.string.category_funny);
 
     private String name;
     private int stringResId;
